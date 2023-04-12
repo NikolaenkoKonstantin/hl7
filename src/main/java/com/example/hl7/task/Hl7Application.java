be5c80c0f36a03d09a98c765fa5c2d1baaf360a8
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-04-12 16:59:16 +0300

    Init

---
+++ src/main/java/com/example/hl7/task/Hl7Application.java
@@ -0,0 +1,13 @@
+package com.example.hl7.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hl7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hl7Application.class, args);
+    }
+
+}
